ea8bea793fc4472af864470263b89c8b31c891f8
DavidCun <99755247+DavidSamCun@users.noreply.github.com>
2022-02-24 17:07:53 -0500

    Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,19 @@ public class Main {
 
     public static void main(String[] args){
 
+        
+        int n;
+        int sum = 0;
+
+        Scanner number = new Scanner(System.in);
+        System.out.print("Enter a number - ");
+        n = number.nextInt();
+        System.out.println("You entered: " + n);
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.print("The sum of all things is " + sum);
     }
 }
